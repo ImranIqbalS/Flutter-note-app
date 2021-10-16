5cb7267fe4357106b82cae6f06b94240acaecc97
ImranIqbalS <imranshaikh5122@gmail.com>
2021-10-16 15:26:07 +0530

    note view completed

---
+++ lib/noteView.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/colors.dart';
+
+class NoteView extends StatefulWidget {
+  const NoteView({Key? key}) : super(key: key);
+
+  @override
+  _NoteViewState createState() => _NoteViewState();
+}
+
+class _NoteViewState extends State<NoteView> {
+  String note =
+      "this is note description this is note description this is note description this is note description this is note description this is note description this is note description";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        backgroundColor: bgColor,
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            splashRadius: 17,
+            onPressed: () {},
+            icon: Icon(
+              Icons.push_pin_outlined,
+            ),
+          ),
+          IconButton(
+            splashRadius: 17,
+            onPressed: () {},
+            icon: Icon(
+              Icons.archive_outlined,
+            ),
+          ),
+          IconButton(
+            splashRadius: 17,
+            onPressed: () {},
+            icon: Icon(
+              Icons.edit_outlined,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Heading",
+              style: TextStyle(
+                color: white,
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              note,
+              style: TextStyle(color: white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
